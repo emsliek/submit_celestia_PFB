a3cbe17bf61f3c786321f4a8934e181836edcf5a
emsliek <emsliekathie036@gmail.com>
2023-05-05 21:07:42 +0800

    submit success

--- main.py
+++ main.py
@@ -5,6 +5,52 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/submit_pfb', methods=['POST'])
+def submit_pfb():
+    """ Create transaction
+    """
+    try:
+        data = request.get_json(force=True)
+        node_url = 'http://127.0.0.1:26659'
+
+        if (
+            not data['message'] or
+            not data['gas_limit'] or
+            not data['fee']
+        ):
+           raise Exception('Insufficient data')
+
+        random_bytes = os.urandom(8)
+        namespace_id = random_bytes.hex()
+        data_hex = data['message'].encode('utf-8').hex()
+
+        tx_submit = requests.post(
+            node_url + '/submit_pfb',
+            json={'namespace_id': namespace_id, 'data': data_hex, 'gas_limit': int(data['gas_limit']), "fee": int(data['fee'])}
+        )
+        tx_submit_data = tx_submit.json()
+
+        print(tx_submit_data)
+
+        if not tx_submit_data['txhash']: 
+            raise Exception('Transaction decline')
+
+        txhash = tx_submit_data['txhash']
+        height = tx_submit_data['height']
+        gas_used = tx_submit_data['gas_used']
+
+        return jsonify({
+                'txhash': txhash,
+                'namespace_id': namespace_id,
+                "data_hex": data,
+                'gas_used': gas_used,
+                'height': height,
+                'code': '200',
+                'date': datetime.datetime.now()
+               })
+
+    except Exception as e:
+        return jsonify({'error': 500, 'message': str(e)}), 500
 
 @app.route('/')
 def index():
--- static/js/main.js
+++ static/js/main.js
@@ -8,12 +8,15 @@
     // create transaction
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
+        document.getElementById("transaction_result").innerHTML = "Please wait...";
 
         let tx_form = {
-            "data": form['data'].value,
+            "message": form['message'].value,
             "gas_limit": form['gas_limit'].value,
             "fee": form['fee'].value,
         };
+
+        console.log(tx_form)
 
         const connect = await fetch(api.submit_pfb, {
             method: 'POST',
@@ -21,25 +24,23 @@
                 'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify(tx_form)
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Data error');
-            }).then(data => {
-                document.getElementById("transaction_result").innerHTML = "Submit Success";
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Data error');
+        }).then(data => {
+            document.getElementById("transaction_result").innerHTML = "Submit Success";
+            document.getElementById("transaction_info").hidden = false
+            document.getElementById("tx_info_tx_hash").innerHTML = data.txhash;
+            document.getElementById("tx_info_height").innerHTML = data.height;
+            document.getElementById("tx_info_date").textContent = data.date;
+            document.getElementById("tx_info_namespace_id").innerHTML = data.namespace_id;
+            document.getElementById("tx_info_gas_used").innerHTML = data.gas_used;
 
-                document.getElementById("tx_info_tx_hash").innerHTML = data.txhash;
-                document.getElementById("tx_info_height").innerHTML = data.height;
-                document.getElementById("tx_info_date").textContent = data.date;
-                document.getElementById("tx_info_namespace_id").innerHTML = data.namespace_id;
-                document.getElementById("tx_info_gas_used").innerHTML = data.gas_used;
-                document.getElementById("tx_info_tx_hash_link").setAttribute("href", api.explorer + "tx/" + data.txhash);
-
-                return data;
-            }).catch(err => {
-                document.getElementById("transaction_result").innerHTML = "Failed, Please make sure the celestia node is available and try again!";
-            });
+            return data;
+        }).catch(err => {
+            document.getElementById("transaction_result").innerHTML = "Failed, Please make sure the celestia node is available and try again!";
+        });
     });
 }
